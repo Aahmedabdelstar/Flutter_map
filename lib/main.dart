ee1b8accab7700c87fea4f086d1b200652981f4e
Ahmed abdelstar <aabdelstar773@gmail.com>
2020-03-20 14:44:17 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:map_launcher/map_launcher.dart';
+
+void main() => runApp(MapLauncherDemo());
+
+class MapLauncherDemo extends StatelessWidget {
+  openMapsSheet(context) async {
+    try {
+      final title = "Google Maps";
+      final description = "Asia's tallest building";
+
+      //open ==>  Al Riyad الرياض السعودية
+      final coords = Coords( 24.633333, 46.716667);
+      final availableMaps = await MapLauncher.installedMaps;
+
+      print(availableMaps);
+
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                child: Wrap(
+                  children: <Widget>[
+                    for (var map in availableMaps)
+                      ListTile(
+                        onTap: () => map.showMarker(
+                          coords: coords,
+                          title: title,
+                          description: description,
+                        ),
+                        title: Text(map.mapName),
+                        leading: Image(
+                          image: map.icon,
+                          height: 30.0,
+                          width: 30.0,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Map Launcher Demo'),
+        ),
+        body: Center(child: Builder(
+          builder: (context) {
+            return MaterialButton(
+              onPressed: () => openMapsSheet(context),
+              child: Text('Show Maps'),
+            );
+          },
+        )),
+      ),
+    );
+  }
+}
